2638a83a2e6258519cabe8eb9ca3efc30aab24c4
Roman Jaquez <romanjaquez@Romans-MacBook-Pro-3.local>
2020-09-29 19:43:20 -0400

    removed my google maps key

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # YouPS - Google Maps on an Angular PWA / Showing Route Polylines Between Two Points
 ### by Roman Just Codes (RJC)
 
-This project is part of a series of projects in which I'll be developing an Angular PWA app for a fictional package delivering company called "YouPS". In this installment, I'm setting up the map and adding custom pins to it.
+This project is part of a series of projects in which I'll be developing an Angular PWA app for a fictional package delivering company called "YouPS". In this installment, I'll be displaying a route between two points in a map.
+
+***NOTE**: YOU MUST HAVE THE GOOGLE DIRECTIONS API ENABLED AS WELL AS A GOOGLE MAPS API KEY FOR THIS TO WORK! More info [here](https://cloud.google.com/maps-platform/routes/?utm_source=google&utm_medium=cpc&utm_campaign=FY18-Q2-global-demandgen-paidsearchonnetworkhouseads-cs-maps_contactsal_saf&utm_content=text-ad-none-none-DEV_c-CRE_289050149247-ADGP_Hybrid+%7C+AW+SEM+%7C+BKWS+~+Google+Maps+Routes+API-KWID_43700038883964686-aud-581578347266:kwd-569137409002-userloc_9001895&utm_term=KW_google%20routes%20api-ST_google+routes+api&gclid=Cj0KCQjwk8b7BRCaARIsAARRTL5UrZgouD8GIumbNRbHSMrxUVO5nvEiZMh62UmY0jn0kvkVuuffwdcaApVaEALw_wcB);
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.0-next.2.
 
--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -9,34 +9,57 @@ import {} from 'googlemaps';
 })
 export class MapPageComponent implements OnInit {
   
+  mapLoaded: boolean;
   map: google.maps.Map;
   center: google.maps.LatLngLiteral;
+
+  source: google.maps.LatLngLiteral;
+  destination: google.maps.LatLngLiteral;
+
   options: google.maps.MapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    scrollwheel: false,
+    scrollwheel: true,
     disableDefaultUI: true,
     disableDoubleClickZoom: true,
-    zoom: 16
+    zoom: 12
   }
 
+  ds: google.maps.DirectionsService;
+  dr: google.maps.DirectionsRenderer;
+
   ngOnInit() {
+
+    this.ds = new google.maps.DirectionsService();
+    this.dr = new google.maps.DirectionsRenderer({
+      map: null,
+      suppressMarkers: true
+    });
+
     navigator.geolocation.getCurrentPosition(position => {
 
-      // getting the center of the map
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
+      this.source = {
+        lat: 37.4220656,
+        lng: -122.0840897
+      };
+
+      this.destination = {
+        lat: 37.342226,
+        lng: -122.0256165
       };
 
       // initialize the map container
       this.map = new google.maps.Map(document.getElementById('map-canvas'), {
         ...this.options,
-        center: this.center
+        center: this.source
+      });
+
+      this.map.addListener('tilesloaded', () => {
+        this.mapLoaded = true;
       });
 
       // adding a marker
       var markerStart = new google.maps.Marker({
-        position: this.center,
+        position: this.source,
         icon: {
           url: './assets/imgs/truck_pin.svg',
           anchor: new google.maps.Point(35,10),
@@ -44,7 +67,37 @@ export class MapPageComponent implements OnInit {
         },
         map: this.map
       });
+
+      var destinationMarker = new google.maps.Marker({
+        position: this.destination,
+        icon: {
+          url: './assets/imgs/destination_custom_pin.svg',
+          anchor: new google.maps.Point(35,10),
+          scaledSize: new google.maps.Size(100, 100)
+        },
+        map: this.map
+      });
+
+      this.setRoutePolyline();
     });
   }
 
+  setRoutePolyline() {
+    let request = {
+      origin: this.source,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    this.ds.route(request, (response, status) => {
+      this.dr.setOptions({
+        suppressPolylines: false,
+        map: this.map
+      });
+
+      if (status == google.maps.DirectionsStatus.OK) {
+        this.dr.setDirections(response);
+      }
+    })
+  }
 }
